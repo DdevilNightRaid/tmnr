a60daa866ace4d663c1196f5cee128d1a5b6d8bd
Devil <ddevil70707@gmail.com>
2022-10-10 22:45:31 +0545

    Created

---
+++ static/js/about.js
@@ -0,0 +1,60 @@
+var moto = document.getElementById('textArea');
+var con = document.getElementById('content');
+var backImg = document.getElementById('mobile')
+function displayIn() {
+    con.style.display = 'none'
+    backImg.style.display = 'none'
+    moto.style.marginTop = '20%'
+    time = setTimeout(displayOut, 15000);
+}
+function displayOut() {
+    moto.style.display = 'none'
+    con.style.display = 'block'
+    backImg.style.display = 'block'
+}
+
+
+
+
+
+
+
+// set up text to print, each item in array is new line
+var aText = new Array(
+    "We Are The MisFits, We Don't Fit In Any Place", 
+    "So We Created Our Own Place To Be Fit In",
+    "&",
+    "It's Known As",
+    "TheMisFitOfNightRaid"
+    );
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+    
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+    
+function typewriter()
+{
+    sContents =  ' ';
+    iRow = Math.max(0, iIndex-iScrollAt);
+    var destination = document.getElementById("typedtext");
+    
+    while ( iRow < iIndex ) {
+    sContents += aText[iRow++] + '<br />';
+    }
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+    if ( iTextPos++ == iArrLength ) {
+    iTextPos = 0;
+    iIndex++;
+    if ( iIndex != aText.length ) {
+    iArrLength = aText[iIndex].length;
+    setTimeout("typewriter()", 500);
+    }
+    } else {
+    setTimeout("typewriter()", iSpeed);
+    }
+}
+typewriter();
